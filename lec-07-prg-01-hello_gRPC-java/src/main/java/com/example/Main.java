bd46906032e45c94190a690b9d0fdee1c33bc617
jsshin <jsshin8128@gmail.com>
2025-11-23 17:58:52 +0900

    feat: lec-07-prg-01-hello_gRPC-java

---
+++ lec-07-prg-01-hello_gRPC-java/src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        if (args.length == 0) {
+            System.out.println("server 또는 client 입력하세요");
+            return;
+        }
+        
+        String mode = args[0].toLowerCase();
+        
+        if (mode.equals("server")) {
+            MyServiceServer.main(new String[0]);
+        } else if (mode.equals("client")) {
+            MyServiceClient.main(new String[0]);
+        } else {
+            System.out.println("잘못 입력했어요");
+        }
+    }
+}
+
